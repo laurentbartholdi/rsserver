d6d36bf59d501aae8cdcb92571d30704999f17eb
aneta-a <aneta.k@gmail.com>
2016-11-23 00:26:57 +0100

    Delete test.js

--- js/test.js
+++
@@ -1,7 +0,0 @@
-console.log("script loaded");
-var para = document.createElement("p");
-var node = document.createTextNode("This is new.");
-para.appendChild(node);
-
-var element = document.getElementById("tests");
-element.appendChild(para);
